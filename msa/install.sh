1fde7ef8a8bf2d77043efb143f967aad7877573f
Gaëtan Harter <gaetan.harter@msasafety.com>
2020-07-09 15:44:16 +0200

    msa: Add many needed libraries

--- msa/install.sh
+++ msa/install.sh
@@ -9,13 +9,30 @@ PROGRAMS+=("ssh-askpass" "gnupg-agent" "gnupg2")
 PROGRAMS+=("i3")
 
 PROGRAMS+=("firefox" "xul-ext-ublock-origin" "flashplugin-installer")
+
 PROGRAMS+=("python3-dev" "python3-pip")
+
+# pyvenv, python -m venv
+PROGRAMS+=("python3-venv")
+# pyenv dependencies to compile python
+PROGRAMS+=("make" "build-essential" "libssl-dev" "zlib1g-dev" "libbz2-dev")
+PROGRAMS+=("libreadline-dev" "libsqlite3-dev" "wget" "curl" "llvm")
+PROGRAMS+=("libncurses5-dev" "xz-utils" "tk-dev" "libxml2-dev" "libxmlsec1-dev")
+PROGRAMS+=("libffi-dev" "liblzma-dev")
 
 PROGRAMS+=("sshfs")
 PROGRAMS+=("mplayer")
 
 PROGRAMS+=("docker.io")
 
+PROGRAMS+=("wget" "curl")
+
+PROGRAMS+=("nmap")
+
 PROGRAMS+=("five-or-more")
 
+sudo apt update
+sudo apt upgrade -y
 sudo apt install ${PROGRAMS[*]}
+
+sudo apt autoremove -y
