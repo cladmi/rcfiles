990403007100fbc5d7a456c295207c1c82f94236
Gaëtan Harter <gaetan.harter@opensynergy.com>
2021-07-02 18:01:54 +0200

    opsy: Allow running install script from anywhere and cleanup deb file.

--- opsy/opsy-deps-gha.install.sh
+++ opsy/opsy-deps-gha.install.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
+SCRIPTDIR=$(dirname $0)
+cd ${SCRIPTDIR}
+
 sudo -v
 
 equivs-build opsy-deps-gha.equivs
-sudo apt-get install --force-reinstall=true ./opsy-deps-gha_1.0_all.deb
+sudo apt-get install --force-reinstall=true ./opsy-deps-gha_*_all.deb
+
+rm opsy-deps-gha_*_all.deb
